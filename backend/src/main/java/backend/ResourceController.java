19767b4c796a923437440be30b83b7cfb9459fb5
Kaushini <kaushiniprabhasha0506@gmail.com>
2026-04-15 11:42:57 +0530

    Complete basic Resource CRUD backend for Module A

---
+++ backend/src/main/java/backend/ResourceController.java
@@ -0,0 +1,46 @@
+package backend;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/resources")
+public class ResourceController {
+
+    @Autowired
+    private ResourceService resourceService;
+
+    @GetMapping
+    public List<Resource> getAllResources() {
+        return resourceService.getAllResources();
+    }
+
+    @GetMapping("/{id}")
+    public Resource getResourceById(@PathVariable Long id) {
+        return resourceService.getResourceById(id);
+    }
+
+    @PostMapping
+    public Resource addResource(@RequestBody Resource resource) {
+        return resourceService.addResource(resource);
+    }
+
+    @PutMapping("/{id}")
+    public Resource updateResource(@PathVariable Long id, @RequestBody Resource resource) {
+        return resourceService.updateResource(id, resource);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteResource(@PathVariable Long id) {
+        resourceService.deleteResource(id);
+    }
+}
+
