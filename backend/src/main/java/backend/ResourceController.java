67e703a007d90cdc9851d00806929397ef3db347
Kaushini <kaushiniprabhasha0506@gmail.com>
2026-04-17 23:04:49 +0530

    Improve Module A UI/UX: add modal edit, delete confirmation, validations, dropdown fields, and refined single-page layout

--- backend/src/main/java/backend/ResourceController.java
+++ backend/src/main/java/backend/ResourceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import backend.entity.Resource;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/resources")
 public class ResourceController {
 
