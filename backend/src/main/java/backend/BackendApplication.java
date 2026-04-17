61e395f13d261ae79ee00c6c23c679ebc50cdda0
ishaninawodya03 <samanprasantha280@gmail.com>
2026-04-17 21:12:00 +0530

    backend start successfully

--- backend/src/main/java/backend/BackendApplication.java
+++ backend/src/main/java/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"backend", "com.smartcampus"})
+@EnableMongoRepositories(basePackages = "com.smartcampus.repository")
 public class BackendApplication {
 
 	public static void main(String[] args) {
