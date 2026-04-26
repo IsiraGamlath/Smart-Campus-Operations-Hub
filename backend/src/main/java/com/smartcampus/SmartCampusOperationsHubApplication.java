5cbf4111b8c67caad7b4c3757f811eed6d719032
Isira Udara Gamlath <it23634008@my.sliit.lk>
2026-04-26 12:23:56 +0530

    Updated project with .env

--- backend/src/main/java/com/smartcampus/SmartCampusOperationsHubApplication.java
+++ backend/src/main/java/com/smartcampus/SmartCampusOperationsHubApplication.java
@@ -1,5 +1,6 @@
 package com.smartcampus;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartCampusOperationsHubApplication {
 
     public static void main(String[] args) {
+
+        // Load .env file
+        Dotenv dotenv = Dotenv.load();
+
+        // Set values to Spring environment
+        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
         SpringApplication.run(SmartCampusOperationsHubApplication.class, args);
     }
 }
-
